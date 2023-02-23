7b899f374e15c246f44279027fc2dc7e1e2fe349
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2023-02-23 11:54:08 -0700

    initial profile section created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './PortfolioContainer/Home/Profile.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Typical from 'react-typical'
+import './Profile.css' 
+//rfc will make boiler plate for component
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    <div className='colz-icon'>
+                        <a href='https://github.com/'>
+                            <i className='fa fa-github-square'></i>
+                        </a>
+                        <a href='https://www.linkedin.com/in/alexmn/'>
+                            <i className='fa fa-linkedin-square'></i>
+                        </a>
+                        <a href='https://www.facebook.com/alex.marie.16547/'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div className='profile-details-name'>
+                    <span className='primary-text'>
+                    {" "}
+                    Hello, I'M <span className="highlighted-text">Alex</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                        <Typical loop={Infinity}
+                            steps={[
+                                "Dynamic Dev ✨",
+                                1200,
+                                "Full Stack Developer 👩🏼‍💻",
+                                1200,
+                                "Vue & React Dev 💻",
+                                1200,
+                                "Music Producer 🎹",
+                                1200,
+                                "Sound Engineer 🎧",
+                                1200,
+                            ]}
+                        />
+                        </h1>
+                        <span className="profile-role-tagline">
+                            Passionate about learning new technologies and building applications with front and back end tools.
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        {" "}
+                        Hire Me{" "}
+                    </button>
+                    <a href='Nunez_Alexandra_Resume.pdf' download='Alexandra Nunez Nunez_Alexandra_Resume.pdf'>
+                        <button className='btn highlighted-btn'>Get Resume </button>
+                    </a>
+                </div>
+            </div>
+            <div className='profile-picture'>
+                <div className='profile-picture-background'>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
